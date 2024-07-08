c1bd82130fa80a1d20209eb7999ce655556b4188
Pranava Chanakya <spranavchanakya@gmail.com>
2024-07-08 01:32:19 -0500

    initial commit

--- src/views/Career.jsx
+++ src/views/Career.jsx
@@ -1,4 +1,4 @@
-// // Timeline.js
+
 // import React from 'react';
 // import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 // import 'react-vertical-timeline-component/style.min.css';
@@ -28,111 +28,262 @@
 //       type: 'career',
 //     },
 //     {
-//       date: 'Jan 2021 - Mar 2022',
+//       date: 'Mar 2021 - Mar 2022',
+//       title: 'research Intern',
+//       location: 'SPCRC, IIITH, India',
+//       description: 'derived and published research conference paper on characterizing SNR chanracteristics in UAV based Communication',
+//       type: 'career',
+//     },
+//     {
+//       date: 'Mar 2021 - Mar 2022',
 //       title: 'Data Scientist',
 //       location: 'Aakarsh Consultancy Enterprises, India',
-//       description: 'Improved ML model accuracy to 89% by refining data structuring and enhancing system reliability...',
+//       description: 'Enhanced ML model accuracy and system reliability, led Azure cloud deployment with key metrics dashboards, improved supplier evaluation, and designed forecasting models for automated inventory management.',
+//       type: 'career',
+//     },
+//     {
+//       date: 'Jan 2020 - Mar 2021',
+//       title: 'Embedded Hardware Intern',
+//       location: 'Aakarsh Consultancy Enterprises, India',
+//       description: 'Worked on Iot and GSM enabled prototypes and built automated embedded devices',
+//       type: 'career',
+//     },
+//     {
+//       date: 'Jun 2019 - Aug 2020',
+//       title: 'Silicon Design Intern',
+//       location: 'Indian Institute of Sciences, India',
+//       description: 'Fabricated passive semiconductor devices and developed, verified digital filters and parity check circuits using VHDL in Xilinx Vivado for high-performance memory subsystems.',
 //       type: 'career',
 //     },
 //     {
 //       date: 'Jul 2021',
 //       title: 'Bachelor of Technology in ECE',
 //       location: 'Sreenidhi Institute of Science and Technology, Hyderabad, India',
-//       description: 'Graduated with a focus on Electronics and Communication Engineering...',
+//       description: 'Graduated in Electronics and Communication Engineering',
 //       type: 'education',
 //     },
 //   ];
 
 //   return (
-//     <VerticalTimeline>
-//       {timelineElements
-//         .sort((a, b) => new Date(b.date) - new Date(a.date)) // Reverse chronological order
-//         .map((element, index) => (
-//           <VerticalTimelineElement
-//             key={index}
-//             date={element.date}
-//             iconStyle={{ background: element.type === 'career' ? 'rgb(33, 150, 243)' : 'rgb(233, 30, 99)', color: '#fff' }}
-//             position={element.type === 'career' ? 'right' : 'left'}
-//           >
-//             <div className="timeline-element-content">
-//               <h3 className="vertical-timeline-element-title">{element.title}</h3>
-//               <h4 className="vertical-timeline-element-subtitle">{element.location}</h4>
-//               <p className="vertical-timeline-element-description">{element.description}</p>
-//             </div>
-//           </VerticalTimelineElement>
-//         ))}
-//     </VerticalTimeline>
+//     <div id="careers">
+//       <VerticalTimeline>
+//         {timelineElements
+//           .sort((a, b) => new Date(b.date) - new Date(a.date)) // Reverse chronological order
+//           .map((element, index) => (
+//             <VerticalTimelineElement
+//               key={index}
+//               date={element.date}
+//               iconStyle={{ background: element.type === 'career' ? 'rgb(33, 150, 243)' : 'rgb(233, 30, 99)', color: '#fff' }}
+//               position={element.type === 'career' ? 'right' : 'left'}
+//             >
+//               <div className="timeline-element-content">
+//                 <h3 className="vertical-timeline-element-title">{element.title}</h3>
+//                 <h4 className="vertical-timeline-element-subtitle">{element.location}</h4>
+//                 <p className="vertical-timeline-element-description">{element.description}</p>
+//               </div>
+//             </VerticalTimelineElement>
+//           ))}
+//       </VerticalTimeline>
+//     </div>
 //   );
 // };
 
 // export default Timeline;
-// Timeline.js
+
+
+
+// import React from 'react';
+
+// import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+// import 'react-vertical-timeline-component/style.min.css';
+
+// import { ReactComponent as WorkIcon } from '../assets/icons/work.svg';
+// import { ReactComponent as SchoolIcon } from '../assets/icons/school.svg';
+// import { ReactComponent as StarIcon } from '../assets/icons/star.svg';
+
+// export default function Home() {
+//     const typeSchool = 'school';
+//     const typeWork = 'work';
+//     const timelineData = [
+//         {
+//             id: 0,
+//             date: 'Aug 2021 - Present',
+//             title: 'Trinity College Dublin – Dublin, Ireland',
+//             subtitle: 'M.Sc. Interactive Digital Media',
+//             type: typeSchool,
+//         },
+//         {
+//             id: 1,
+//             date: 'Oct 2019 – July 2021',
+//             title: 'JOSH TECHNOLOGY GROUP – Gurgaon, India',
+//             subtitle: 'Senior Software Developer - 1',
+//             type: typeWork,
+//         },
+//         {
+//             id: 2,
+//             date: 'July 2018 – Sep 2019',
+//             title: 'JOSH TECHNOLOGY GROUP – Gurgaon, India',
+//             subtitle: 'Software Developer',
+//             type: typeWork,
+//         },
+//         {
+//             id: 3,
+//             date: 'Jan 2018 – Jun 2018  ',
+//             title: 'JOSH TECHNOLOGY GROUP – Gurgaon, India',
+//             subtitle: 'Software Development Intern',
+//             type: typeWork,
+//         },
+//         {
+//             id: 4,
+//             date: 'Aug 2014 - July 2018',
+//             title: 'YMCA University of Science and Technology – Faridabad, India',
+//             subtitle: 'Bachelor of Technology, Computer Engineering',
+//             type: typeSchool,
+//         },
+//         {
+//             id: 5,
+//             date: 'Apr 2014',
+//             title: 'Delhi Public School, Sushant Lok – Gurgaon, India',
+//             subtitle: 'High School',
+//             type: typeSchool,
+//         },
+//     ];
+
+//     return (
+//         <VerticalTimeline
+//             className="vertical-timeline-container"
+//             animate={false}
+//             lineColor="#000"
+//         >
+//             {timelineData.map(item => (
+//                     <VerticalTimelineElement
+//                         id={item.id}
+//                         key={item.id}
+//                         className="vertical-timeline-element--work"
+//                         contentStyle={{ background: '#aaa', color: '#000' }}
+//                         contentArrowStyle={{ borderRight: '7px solid #aaa' }}
+//                         date={item.date}
+//                         iconStyle={{ background: '#aaa', border: '#000' }}
+//                         icon={item.type === typeWork ? <WorkIcon /> : <SchoolIcon />}
+//                     >
+//                         <h3 className="vertical-timeline-element-title">{item.title}</h3>
+//                         <h4 className="vertical-timeline-element-subtitle">{item.subtitle}</h4>
+//                     </VerticalTimelineElement>
+//                 ))}
+//             <VerticalTimelineElement
+//                 id="vertical-timeline-icon-star"
+//                 iconStyle={{ background: '#aaa', border: '#000' }}
+//                 icon={<StarIcon />}
+//             />
+//         </VerticalTimeline>
+//     );
+// }
+
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import './Timeline.css'; // Add custom styles for hover effect
 
-const Timeline = () => {
-  const timelineElements = [
-    {
-      date: 'Feb 2024 - Present',
-      title: 'Machine Learning Engineer',
-      location: 'MeghaAI, Indiana, USA',
-      description: 'Slashed business operations costs by 50% by innovating an AI model using Regression for predictive maintenance of RO unit filters...',
-      type: 'career',
-    },
-    {
-      date: 'Dec 2023',
-      title: 'Master of Science in ECE',
-      location: 'University of Wisconsin-Madison',
-      description: 'Specialized in Machine Learning and Signal Processing...',
-      type: 'education',
-    },
-    {
-      date: 'Apr 2022 - Jul 2022',
-      title: 'Program Analyst Trainee',
-      location: 'Cognizant Technology Solutions, India',
-      description: 'Examined large multivariant datasets, engineered, and deployed ETL and ML pipelines using Docker...',
-      type: 'career',
-    },
-    {
-      date: 'Jan 2021 - Mar 2022',
-      title: 'Data Scientist',
-      location: 'Aakarsh Consultancy Enterprises, India',
-      description: 'Improved ML model accuracy to 89% by refining data structuring and enhancing system reliability...',
-      type: 'career',
-    },
-    {
-      date: 'Jul 2021',
-      title: 'Bachelor of Technology in ECE',
-      location: 'Sreenidhi Institute of Science and Technology, Hyderabad, India',
-      description: 'Graduated with a focus on Electronics and Communication Engineering...',
-      type: 'education',
-    },
-  ];
+import { ReactComponent as WorkIcon } from '../assets/icons/work.svg';
+import { ReactComponent as SchoolIcon } from '../assets/icons/school.svg';
+import { ReactComponent as StarIcon } from '../assets/icons/star.svg';
 
-  return (
-    <div id="careers">
-      <VerticalTimeline>
-        {timelineElements
-          .sort((a, b) => new Date(b.date) - new Date(a.date)) // Reverse chronological order
-          .map((element, index) => (
+export default function Home() {
+    const typeSchool = 'school';
+    const typeWork = 'work';
+    const timelineData = [
+        {
+            id: 0,
+            date: 'Feb 2024 - Present',
+            title: 'Machine Learning Engineer',
+            subtitle: 'MeghaAI, Indiana, USA',
+            description: 'Slashed business operations costs by 50% by innovating an AI model using Regression for predictive maintenance of RO unit filters...',
+            type: typeWork,
+        },
+        {
+            id: 1,
+            date: 'Dec 2023',
+            title: 'Master of Science in ECE',
+            subtitle: 'University of Wisconsin-Madison',
+            description: 'Specialized in Machine Learning and Signal Processing...',
+            type: typeSchool,
+        },
+        {
+            id: 2,
+            date: 'Apr 2022 - Jul 2022',
+            title: 'Program Analyst Trainee',
+            subtitle: 'Cognizant Technology Solutions, India',
+            description: 'Examined large multivariant datasets, engineered, and deployed ETL and ML pipelines using Docker...',
+            type: typeWork,
+        },
+        {
+            id: 3,
+            date: 'Mar 2021 - Mar 2022',
+            title: 'Research Intern',
+            subtitle: 'SPCRC, IIITH, India',
+            description: 'Derived and published a research conference paper on characterizing SNR characteristics in UAV-based communication...',
+            type: typeWork,
+        },
+        {
+            id: 4,
+            date: 'Mar 2021 - Mar 2022',
+            title: 'Data Scientist',
+            subtitle: 'Aakarsh Consultancy Enterprises, India',
+            description: 'Enhanced ML model accuracy and system reliability, led Azure cloud deployment with key metrics dashboards, improved supplier evaluation, and designed forecasting models for automated inventory management...',
+            type: typeWork,
+        },
+        {
+            id: 7,
+            date: 'Jul 2021',
+            title: 'Bachelor of Technology in ECE',
+            subtitle: 'Sreenidhi Institute of Science and Technology, Hyderabad, India',
+            description: 'Graduated in Electronics and Communication Engineering...',
+            type: typeSchool,
+        },
+        {
+            id: 5,
+            date: 'Jan 2020 - Mar 2021',
+            title: 'Embedded Hardware Intern',
+            subtitle: 'Aakarsh Consultancy Enterprises, India',
+            description: 'Worked on IoT and GSM-enabled prototypes and built automated embedded devices...',
+            type: typeWork,
+        },
+        {
+            id: 6,
+            date: 'Jun 2019 - Aug 2020',
+            title: 'Silicon Design Intern',
+            subtitle: 'Indian Institute of Sciences, India',
+            description: 'Fabricated passive semiconductor devices and developed, verified digital filters and parity check circuits using VHDL in Xilinx Vivado for high-performance memory subsystems...',
+            type: typeWork,
+        },
+    ];
+
+    return (
+        <VerticalTimeline
+            className="vertical-timeline-container"
+            animate={false}
+            lineColor="#000"
+        >
+            {timelineData.map(item => (
+                <VerticalTimelineElement
+                    id={item.id}
+                    key={item.id}
+                    className="vertical-timeline-element--work"
+                    contentStyle={{ background: '#aaa', color: '#000' }}
+                    contentArrowStyle={{ borderRight: '7px solid #aaa' }}
+                    date={item.date}
+                    iconStyle={{ background: item.type === typeWork ? '#aaa' : '#e91e63', border: '#000' }}
+                    icon={item.type === typeWork ? <WorkIcon /> : <SchoolIcon />}
+                >
+                    <h3 className="vertical-timeline-element-title">{item.title}</h3>
+                    <h4 className="vertical-timeline-element-subtitle">{item.subtitle}</h4>
+                    <p>{item.description}</p>
+                </VerticalTimelineElement>
+            ))}
             <VerticalTimelineElement
-              key={index}
-              date={element.date}
-              iconStyle={{ background: element.type === 'career' ? 'rgb(33, 150, 243)' : 'rgb(233, 30, 99)', color: '#fff' }}
-              position={element.type === 'career' ? 'right' : 'left'}
-            >
-              <div className="timeline-element-content">
-                <h3 className="vertical-timeline-element-title">{element.title}</h3>
-                <h4 className="vertical-timeline-element-subtitle">{element.location}</h4>
-                <p className="vertical-timeline-element-description">{element.description}</p>
-              </div>
-            </VerticalTimelineElement>
-          ))}
-      </VerticalTimeline>
-    </div>
-  );
-};
-
-export default Timeline;
+                id="vertical-timeline-icon-star"
+                iconStyle={{ background: '#aaa', border: '#000' }}
+                icon={<StarIcon />}
+            />
+        </VerticalTimeline>
+    );
+}
--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,11 +1,89 @@
 
-import React, { useContext } from "react";
+// import React, { useContext } from "react";
+// import Card from "../components/Card";
+// import { ThemeContext } from "../themeProvider";
+
+// const Projects = () => {
+//   const theme = useContext(ThemeContext);
+//   const darkMode = theme.state.darkMode;
+
+//   const projectData = [
+//     {
+//       url: "",
+//       imgSrc: "https://cdn.jsdelivr.net/gh/spchanakya/spchanakya.github.io@main/Assets/image_chatbot.jpg",
+//       title: "Generative AI-Enabled Web Application for SQL Databases",
+//       associatedWith: "MeghaAI",
+//       skills: "Python, GenAI, PostgreSQL, RAG, Prompt Engineering",
+//       description: "Developed an Chat-interface powered by ChatGPT3.5 turbo on top of SQL (PostgreSQL DB). Utilized RAG and Prompt engineering techniques to ehnace the relavance factor by 3x"
+//     },
+//     {
+//       url: "",
+//       imgSrc: "https://cdn.jsdelivr.net/gh/spchanakya/spchanakya.github.io@main/Assets/image_grafana.jpg",
+//       title: "KPI visualizations and monitoring using Grafana",
+//       associatedWith: "MeghaAI",
+//       skills: "Grafana, JavaScript, Apache Echarts",
+//       description: `Analyzed large MES Data in PostgreSQL, Provided Dashboards for critical KPI metrics in Grafana using Apache charts,
+//       Plotly to enhance inventory management, and Initiated cloud data sourcing methods for real-time performance visualization.`
+//     },
+//     {
+//       url: "/project1-url",
+//       imgSrc: "https://cdn.jsdelivr.net/gh/spchanakya/spchanakya.github.io@main/Assets/image_RO.jpg",
+//       title: "Predictive Maintainance of RO filters",
+//       associatedWith: "MeghaAI",
+//       skills: "Python, ML, Time series, CNN",
+//       description: `Slashed Business operations costs by 50% in the Plant by Innovating an AI model using Regression, performed correlation
+// analysis for feature engineering with sensor data for predictive maintenance of stakeholders’ $50 million RO unit filters.`
+//     },
+//     {
+//       url: "/project2-url",
+//       imgSrc: "https://cdn.jsdelivr.net/gh/spchanakya/spchanakya.github.io@main/Assets/image_capstone.jpg",
+//       title: "Anhanced MobilenetV2 for fruit and vegetable quality classification",
+//       associatedWith: "University of Wisconsin - Madison",
+//       skills: "Python, Computer vision, Neural network, Tensorflow, ",
+//       description: "This is a short description of Project 2."
+//     }
+//   ];
+
+//   return (
+//     <div
+//       id="projects"
+//       className={darkMode ? "bg-white text-black" : "bg-gray-900 text-white"}
+//     >
+//       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-12">
+//         <h2 className="text-5xl font-bold px-4 md:px-0 text-center">
+//           Projects
+//         </h2>
+//         <h4 className="mt-16 text-3xl font-semibold text-blue-600">
+//           What I Built
+//         </h4>
+//         <div className="mt-8 flex flex-wrap -mx-4">
+//           {projectData.map((project, index) => (
+//             <Card
+//               key={index}
+//               url={project.url}
+//               imgSrc={project.imgSrc}
+//               title={project.title}
+//               associatedWith={project.associatedWith}
+//               skills={project.skills}
+//               description={project.description}
+//             />
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Projects;
+
+import React, { useContext, useState } from "react";
 import Card from "../components/Card";
 import { ThemeContext } from "../themeProvider";
 
 const Projects = () => {
   const theme = useContext(ThemeContext);
   const darkMode = theme.state.darkMode;
+  const [selectedCategory, setSelectedCategory] = useState("All");
 
   const projectData = [
     {
@@ -14,7 +92,8 @@ const Projects = () => {
       title: "Generative AI-Enabled Web Application for SQL Databases",
       associatedWith: "MeghaAI",
       skills: "Python, GenAI, PostgreSQL, RAG, Prompt Engineering",
-      description: "Developed an Chat-interface powered by ChatGPT3.5 turbo on top of SQL (PostgreSQL DB). Utilized RAG and Prompt engineering techniques to ehnace the relavance factor by 3x"
+      description: "Developed an Chat-interface powered by ChatGPT3.5 turbo on top of SQL (PostgreSQL DB). Utilized RAG and Prompt engineering techniques to ehnace the relavance factor by 3x",
+      category: "ML"
     },
     {
       url: "",
@@ -23,41 +102,82 @@ const Projects = () => {
       associatedWith: "MeghaAI",
       skills: "Grafana, JavaScript, Apache Echarts",
       description: `Analyzed large MES Data in PostgreSQL, Provided Dashboards for critical KPI metrics in Grafana using Apache charts,
-      Plotly to enhance inventory management, and Initiated cloud data sourcing methods for real-time performance visualization.`
+      Plotly to enhance inventory management, and Initiated cloud data sourcing methods for real-time performance visualization.`,
+      category: "ML"
     },
     {
-      url: "/project1-url",
+      url: "",
+      imgSrc: "https://cdn.jsdelivr.net/gh/spchanakya/spchanakya.github.io@main/Assets/image_grafana.jpg",
+      title: "KPI visualizations and monitoring using Grafana",
+      associatedWith: "MeghaAI",
+      skills: "Grafana, JavaScript, Apache Echarts",
+      description: `Analyzed large MES Data in PostgreSQL, Provided Dashboards for critical KPI metrics in Grafana using Apache charts,
+      Plotly to enhance inventory management, and Initiated cloud data sourcing methods for real-time performance visualization.`,
+      category: "ML"
+    },
+    {
+      url: "/",
       imgSrc: "https://cdn.jsdelivr.net/gh/spchanakya/spchanakya.github.io@main/Assets/image_RO.jpg",
       title: "Predictive Maintainance of RO filters",
       associatedWith: "MeghaAI",
       skills: "Python, ML, Time series, CNN",
       description: `Slashed Business operations costs by 50% in the Plant by Innovating an AI model using Regression, performed correlation
-analysis for feature engineering with sensor data for predictive maintenance of stakeholders’ $50 million RO unit filters.`
+analysis for feature engineering with sensor data for predictive maintenance of stakeholders’ $50 million RO unit filters.`,
+      category: "ML"
     },
     {
-      url: "/project2-url",
+      url: "/",
       imgSrc: "https://cdn.jsdelivr.net/gh/spchanakya/spchanakya.github.io@main/Assets/image_capstone.jpg",
-      title: "Project 2",
-      associatedWith: "MeghaAI",
-      skills: "Python, GenAI, PostgreSQL, RAG, Prompt Engineering",
-      description: "This is a short description of Project 2."
-    }
+      title: "Anhanced MobilenetV2 for fruit and vegetable quality classification",
+      associatedWith: "University of Wisconsin - Madison",
+      skills: "Python, Computer vision, Neural network, Tensorflow, ",
+      description: "This is a short description of Project 2.",
+      category: "ML"
+    },
+    {
+      url: "",
+      imgSrc: "https://cdn.jsdelivr.net/gh/spchanakya/spchanakya.github.io@main/Assets/image_grafana.jpg",
+      title: "Model parameter tuning using MLFlow",
+      associatedWith: "Akarsh Consultancy Enterprises",
+      skills: "Grafana, JavaScript, Apache Echarts",
+      description: `Analyzed large MES Data in PostgreSQL, Provided Dashboards for critical KPI metrics in Grafana using Apache charts,
+      Plotly to enhance inventory management, and Initiated cloud data sourcing methods for real-time performance visualization.`,
+      category: "ML"
+    },
+    {
+      url: "",
+      imgSrc: "https://cdn.jsdelivr.net/gh/spchanakya/spchanakya.github.io@main/Assets/image_grafana.jpg",
+      title: "KPI visualizations and monitoring using Grafana",
+      associatedWith: "Cognizant Solutions",
+      skills: "Grafana, JavaScript, Apache Echarts",
+      description: `Analyzed large MES Data in PostgreSQL, Provided Dashboards for critical KPI metrics in Grafana using Apache charts,
+      Plotly to enhance inventory management, and Initiated cloud data sourcing methods for real-time performance visualization.`,
+      category: "ML"
+    },
   ];
 
+  const categories = ["All", "ML", "Embedded"];
+
+  const filteredProjects = selectedCategory === "All" ? projectData : projectData.filter(project => project.category === selectedCategory);
+
   return (
-    <div
-      id="projects"
-      className={darkMode ? "bg-white text-black" : "bg-gray-900 text-white"}
-    >
+    <div id="projects" className={darkMode ? "bg-white text-black" : "bg-gray-900 text-white"}>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-12">
-        <h2 className="text-5xl font-bold px-4 md:px-0 text-center">
-          Projects
-        </h2>
-        <h4 className="mt-16 text-3xl font-semibold text-blue-600">
-          What I Built
-        </h4>
+        <h2 className="text-5xl font-bold px-4 md:px-0 text-center">Projects</h2>
+        <h4 className="mt-16 text-3xl font-semibold text-blue-600">What I Built</h4>
+        <div className="mt-8 flex justify-center">
+          {categories.map(category => (
+            <button
+              key={category}
+              className={`mx-2 px-4 py-2 ${selectedCategory === category ? 'bg-blue-600 text-white' : 'bg-gray-200 text-black'} rounded`}
+              onClick={() => setSelectedCategory(category)}
+            >
+              {category}
+            </button>
+          ))}
+        </div>
         <div className="mt-8 flex flex-wrap -mx-4">
-          {projectData.map((project, index) => (
+          {filteredProjects.map((project, index) => (
             <Card
               key={index}
               url={project.url}
@@ -75,3 +195,4 @@ analysis for feature engineering with sensor data for predictive maintenance of 
 };
 
 export default Projects;
+
